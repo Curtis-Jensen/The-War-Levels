b0f9c67d67b7f878de536a54600e370d55b58d75
maeserhp96 <55401732+maeserhp96@users.noreply.github.com>
2020-02-06 17:51:33 -0700

    Timer and score tracker added
    I still need to make sure the timer stops when the nephites all die

--- The War Levels/Assets/Scripts/ScoreManager.cs
+++ The War Levels/Assets/Scripts/ScoreManager.cs
@@ -1,17 +1,19 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
-public class ScoreDisplay : MonoBehaviour
+public class ScoreManager : MonoBehaviour
 {
 
-    public Text score; //Represents the score of of the current game being played
-    public Text highScore; //Represents the highest score that the player has acchived in a single game
+    public int score; //Represents the score of of the current game being played
+    public int highScore;
+    //public Text scoreText;
+    //public Text highScoreText; //Represents the highest score that the player has acchived in a single game
 
     // Start is called before the first frame update
     void Start()
     {
-        score.text = PlayerPrefs.GetInt("score", 0).ToString();
-        highScore.text = PlayerPrefs.GetInt("highScore", 0).ToString();
+        score = PlayerPrefs.GetInt("score", 0);
+        highScore = PlayerPrefs.GetInt("highScore", 0);
     }
 
     // Update is called once per frame
@@ -26,12 +28,12 @@ public class ScoreDisplay : MonoBehaviour
     public void Reset()
     {
         PlayerPrefs.DeleteKey("score");
-        score.text = "0";
+        //score.text = "0";
         PlayerPrefs.DeleteKey("HighScore");
-        highScore.text = "0";
+        //highScore.text = "0";
     }
 
-    public static void UpdateHighScore(int score)
+    public static void updateHighScore(int score)
     {
         if (score > PlayerPrefs.GetInt("highScore", 0))
         {
@@ -41,7 +43,7 @@ public class ScoreDisplay : MonoBehaviour
 
     //I am pretty sure this method will not be needed
     //If it is needed the code almost definately needs to be changed
-    public static void UpdateTotalMoney(int score)
+    public static void updateTotalMoney(int score)
     {
         score = score + PlayerPrefs.GetInt("score", 0);
         PlayerPrefs.SetInt("score", score);
---
+++ The War Levels/Assets/Timer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+
+    public Text timerText;
+    private float startTime;
+    private bool finished = false;
+    private float t;
+    //public ScoreManager scoreManager = new ScoreManager();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(finished)
+        {
+            return;
+        }
+        else
+        {
+            t = Time.time - startTime;
+            string minutes = ((int)t / 60).ToString();
+            string seconds = (t % 60).ToString("f2");
+            timerText.text = minutes + ":" + seconds;
+        }
+       
+    }
+
+    public void Finish()
+    {
+        finished = true;
+        timerText.color = Color.yellow;
+        ScoreManager.updateHighScore((int)t);
+        
+    }
+}
