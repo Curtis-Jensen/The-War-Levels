1bf07821fb039ead4080727314b34dd1488d770c
Curtis-Jensen <38483758+Curtis-Jensen@users.noreply.github.com>
2020-02-19 16:36:25 -0700

    Revert "EnemySpawner"
    This reverts commit 9906e4aa8c2e5a787eca362b7136ee60a4a57685.

--- The War Levels/Assets/Scripts/Army.cs
+++ The War Levels/Assets/Scripts/Army.cs
@@ -64,7 +64,7 @@ public abstract class Army : MonoBehaviour
         }
     }
 
-    public void Shrink(int damage)
+    void Shrink(int damage)
     {
         Vector3 theScale = transform.localScale;//Makes the vector to shrink with
         theScale.x -= (.0005f * (float)damage);
