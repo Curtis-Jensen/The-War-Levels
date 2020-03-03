6c69cfe0cf5fb0b03a7cbf190dec589f44ea30c0
Curtis-Jensen <38483758+Curtis-Jensen@users.noreply.github.com>
2020-03-02 18:02:36 -0700

    Fixed navigation
    I guess I changed spear too?

--- The War Levels/Assets/Scripts/ClickManager.cs
+++ The War Levels/Assets/Scripts/ClickManager.cs
@@ -6,17 +6,27 @@ public class ClickManager : MonoBehaviour
 {
     public NArmy armySubject;
 
+    private Vector3 clickPosition;
+
+    /* When the player clicks this object's location goes there.
+     * When this object's location goes there it makes the army follow.
+     */
     void Update()
     {
         if (Input.GetMouseButtonDown(1) && armySubject.selected)
         {
-            Vector3 clickPosition = -Vector3.one;
-
-            clickPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition + new Vector3(0, 0, 5f));
-
-            transform.position = clickPosition;//By changing this object's position, the 
-            
-
+            clickPosition = GetWorldPositionOnPlane(Input.mousePosition, 0);
+            Debug.Log(Input.mousePosition + " | " + clickPosition);
+            transform.position = clickPosition; 
         }
+    }
+
+    public Vector3 GetWorldPositionOnPlane(Vector3 screenPosition, float z)
+    {
+        Ray ray = Camera.main.ScreenPointToRay(screenPosition);
+        Plane xy = new Plane(Vector3.forward, new Vector3(0, 0, z));
+        float distance;
+        xy.Raycast(ray, out distance);
+        return ray.GetPoint(distance);
     }
 }
