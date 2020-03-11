f0fcffd52cdd396db603f0976e2fc6464409c7cf
cavalcante2019 <eldercavalcanterpm@gmail.com>
2020-03-10 23:21:11 -0600

    Camera movement
    It is working pretty fine guys.

---
+++ The War Levels/Assets/Move_Cam.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move_Cam : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
