5d72aaf53fb52a150509aa14387f18c851c9f459
Curtis-Jensen <38483758+Curtis-Jensen@users.noreply.github.com>
2020-03-18 16:44:37 -0600

    Fixed some naming

--- The War Levels/Assets/Scripts/ArmyScripts/LamaniteTargeter.cs
+++ The War Levels/Assets/Scripts/ArmyScripts/LamaniteTargeter.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class LamaniteTargeter : MonoBehaviour
 {
-    public Transform myTransform;
+    public Transform armySubject;
 
     static Transform[] possibleTargets;
 
