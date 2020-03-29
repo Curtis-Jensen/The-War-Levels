5f64777f440fe2439a38b391a82a5faf0366f419
Curtis-Jensen <38483758+Curtis-Jensen@users.noreply.github.com>
2020-03-29 17:56:09 -0600

    The Lamanites navigate correctly when spawning

--- The War Levels/Assets/Scripts/ArmyScripts/LArmy.cs
+++ The War Levels/Assets/Scripts/ArmyScripts/LArmy.cs
@@ -88,11 +88,14 @@ public class LArmy : Army
 
     /* The Lamanites spawn new Lamanites when they die because they were "innumerable" in
      * the battle, so they are infinite in code.
+     * 
+     * Makes sure the name is correct for navigation purposes.
      */
     protected override void Die()
     {
         GameObject lamanite = Instantiate(lamanite_army, new Vector3(5.478f, -0.687f, 0), Quaternion.identity);
         lamanite.GetComponent<LArmy>().soldierNumber = 10000;
+        lamanite.name = name;
         base.Die();
     }
 }
