115656c2551d1570d4160426f1d81798ce5f7ac5
cavalcante2019 <eldercavalcanterpm@gmail.com>
2020-03-29 21:48:18 -0600

    Main menu part 1

---
+++ The War Levels/Assets/Sharp Text Mesh/Script/SharpText.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SharpText : MonoBehaviour {
+
+    public float sizeInUnits; // Set size of text in units. Most fonts have a lot of empty space above and below the characters. So it won't probably match the grid.
+    public TextMesh textMesh; //In inspector assign the Text Mesh component
+
+    public Camera mainCamera;
+    private float sharpness;
+	
+	// Update is called once per frame
+	void Start() {
+
+        OnCameraSizeChange();
+	}
+
+    public void OnCameraSizeChange()//call this function everytime you change camera size
+    {
+        sharpness = Screen.height / (20 * mainCamera.orthographicSize);
+        textMesh.fontSize = Mathf.RoundToInt(sharpness * sizeInUnits);
+        textMesh.characterSize = 1 / (float)sharpness;
+    }
+}
---
+++ The War Levels/Assets/Sharp Text Mesh/Script/SharpText_Demo.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SharpText_Demo : MonoBehaviour {
+
+    //This script showcases what SharpTextMesh does.
+    //It updates every frame to adjust to changing  the camera size in editor. Therefore it slows down runtime performance.
+    //For normal usage use SharpText.cs
+
+    public float sizeInUnits; // Set size of text in units. Most fonts have a lot of empty space above and below the characters. So it won't probably match the grid.
+    public TextMesh textMesh; //In inspector assign the Text Mesh component
+    public Camera mainCamera;
+
+    private float sharpness;
+	
+	// Update is called once per frame
+	void Update() {
+
+        sharpness = Screen.height / (20 * mainCamera.orthographicSize);
+        textMesh.fontSize = Mathf.RoundToInt(sharpness*sizeInUnits);
+        textMesh.characterSize = 1/(float)sharpness;
+
+	}
+}
