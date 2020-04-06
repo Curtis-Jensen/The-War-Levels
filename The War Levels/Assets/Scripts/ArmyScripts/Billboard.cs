6793e4353e32477c0cf077a757838193f6672359
Mason Nielson <masonandmckay@gmail.com>
2020-04-06 16:14:15 -0600

    Started billboarding.

---
+++ The War Levels/Assets/Scripts/ArmyScripts/Billboard.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    public Transform main_camera_transform;
+    void Start()
+    {
+        main_camera_transform = FindObjectOfType<Camera>().transform;
+    }
+    void Update()
+    {
+        this.transform.LookAt(main_camera_transform);
+    }
+}
