925abe07c672c4f07225ca1b3d49396cc5d65cf7
Curtis-Jensen <me@curtisjensen.com>
2024-02-28 20:09:12 -0700

    moved some folder, trying to do grid stuff as well

---
+++ The War Levels/Assets/GridManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GridManager : MonoBehaviour
+{
+    public int width, height;
+    public Tile tilePrefab;
+
+    void GenerateGrid()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+
+            }
+        }
+    }
+}
